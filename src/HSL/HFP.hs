ff5076b633487dad5db6684fbfd9164f161fa9fd
Piotr Majkrzak <petrol.91@gmail.com>
2019-08-01 19:29:48 +0300

    Add High-frequency positioning (HFP 2.0) message feed
    To be fixed cause it does not work :shrug:

---
+++ src/HSL/HFP.hs
@@ -0,0 +1,36 @@
+module HSL.HFP where
+
+import Data.Aeson (FromJSON, parseJSON, withObject, (.:), Value(Object), decode)
+import Data.HashMap.Strict (elems)
+import Control.Concurrent
+import Network.MQTT.Client (mqttConfig, _msgCB, _hostname, runClient, subscribe, waitForClient, QoS(QoS0))
+import Data.ByteString.Lazy (ByteString)
+
+data Message = Message {
+  operator :: Integer,
+  vehicle :: Integer,
+  stop :: Integer
+} deriving Show
+
+
+instance FromJSON Message where
+  parseJSON (Object o ) = parseJSON' (head (elems o))
+   where parseJSON' (Object o) = Message <$> o .: "oper" <*> o .: "veh" <*> o .: "stop"
+
+
+messages :: IO [Message]
+messages = do
+ msg <- newEmptyMVar
+ mc <- runClient mqttConfig{
+   _hostname = "mqtt.hsl.fi",
+   _msgCB = Just (\_ t m -> putMVar msg m)
+ }
+ subscribe mc [("/hfp/v2/journey/ongoing/arr/#", QoS0)]
+ messages' msg
+ where
+  messages' :: MVar ByteString -> IO [Message]
+  messages' msg = do
+    payload <- takeMVar msg
+    case decode payload :: Maybe Message of
+     Just message -> (message :) <$> messages' msg
+     Nothing -> messages' msg
