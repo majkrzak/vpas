acc5a853d03f6696d6960d79d1af4554529b120d
Piotr Majkrzak <petrol.91@gmail.com>
2019-08-03 16:28:02 +0300

    Apply formating

--- src/HSL/HFP.hs
+++ src/HSL/HFP.hs
@@ -1,42 +1,56 @@
 module HSL.HFP where
 
-import Data.Aeson (FromJSON, parseJSON, withObject, (.:), Value(Object), decode)
-import Data.HashMap.Strict (elems)
 import Control.Concurrent
-import Network.MQTT.Client (mqttConfig, _msgCB, _hostname, runClient, subscribe, waitForClient, QoS(QoS0))
-import Data.ByteString.Lazy (ByteString)
-import GHC.IO.Unsafe (unsafeInterleaveIO)
+import Data.Aeson
+       ((.:)
+       ,FromJSON
+       ,Value(Object)
+       ,decode
+       ,parseJSON
+       ,withObject)
+import Data.ByteString.Lazy
+       (ByteString)
+import Data.HashMap.Strict
+       (elems)
+import GHC.IO.Unsafe
+       (unsafeInterleaveIO)
+import Network.MQTT.Client
+       (QoS(QoS0)
+       ,_hostname
+       ,_msgCB
+       ,mqttConfig
+       ,runClient
+       ,subscribe
+       ,waitForClient)
 
-data Message = Message {
-  operator :: Integer,
-  vehicle :: Integer,
-  stop :: Integer
-} deriving Show
-
+data Message =
+  Message
+  { operator :: Integer
+  , vehicle :: Integer
+  , stop :: Integer
+  }
+  deriving Show
 
 instance FromJSON Message where
   parseJSON (Object o) = parseJSON' . head . elems $ o
-   where
-    parseJSON' (Object o) =
-     Message
-      <$> o .: "oper"
-      <*> o .: "veh"
-      <*> o .: "stop"
-
+    where
+      parseJSON' (Object o) =
+        Message <$> o .: "oper" <*> o .: "veh" <*> o .: "stop"
 
 messages :: IO [Message]
 messages = do
- msg <- newEmptyMVar
- mc <- runClient mqttConfig{
-   _hostname = "mqtt.hsl.fi",
-   _msgCB = Just (\_ t m -> putMVar msg m)
- }
- subscribe mc [("/hfp/v2/journey/ongoing/arr/#", QoS0)]
- messages' msg
- where
-  messages' :: MVar ByteString -> IO [Message]
-  messages' msg = unsafeInterleaveIO $ do
-    payload <- takeMVar msg
-    case decode payload :: Maybe Message of
-     Just message -> (message :) <$> messages' msg
-     Nothing -> messages' msg
+  msg <- newEmptyMVar
+  mc <- runClient
+    mqttConfig
+      { _hostname = "mqtt.hsl.fi"
+      , _msgCB = Just (\_ t m -> putMVar msg m)
+      }
+  subscribe mc [("/hfp/v2/journey/ongoing/arr/#", QoS0)]
+  messages' msg
+  where
+    messages' :: MVar ByteString -> IO [Message]
+    messages' msg = unsafeInterleaveIO $ do
+      payload <- takeMVar msg
+      case decode payload :: Maybe Message of
+        Just message -> (message :) <$> messages' msg
+        Nothing -> messages' msg
