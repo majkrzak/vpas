a0e7c9b060b3d5862b32fa7a174c103e1585ae91
Piotr Majkrzak <petrol.91@gmail.com>
2019-08-06 22:20:17 +0300

    Fix style

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,14 @@
 module Main where
 
 import HSL.HFP
+import Jumper
 import System.IO
 import Util.Stream
-import Jumper
 
 loop event state = do
   hFlush stdout
   case update state event of
     Just next' -> print next' >> return next'
     Nothing -> return state
-
 
 main = foldStream loop Dangling =<< events
