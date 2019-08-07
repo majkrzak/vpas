b2aef12b7f05ceab42b214d99c9d01630eb37961
Piotr Majkrzak <petrol.91@gmail.com>
2019-08-08 00:35:15 +0300

    Implement functor

--- src/Util/Stream.hs
+++ src/Util/Stream.hs
@@ -1,4 +1,4 @@
-module Util.Stream (Stream,runStream,mapStream) where
+module Util.Stream (Stream,runStream) where
 
 import Control.Concurrent.STM
 import System.IO.Unsafe
@@ -16,8 +16,8 @@ runStream block = do
 instance Foldable Stream where
   foldMap f (Stream read) = f (unsafePerformIO $ atomically read) <> foldMap f (Stream read)
 
-mapStream :: (a -> b) -> Stream a -> Stream b
-mapStream f (Stream read) = Stream $ f <$> read
+instance Functor Stream where
+  fmap f (Stream read) = Stream $ f <$> read
 
 zipStream :: [Stream a] -> Stream a
 zipStream = undefined
